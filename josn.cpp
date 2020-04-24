f5b12fa6fe51cfdae65261c1352a4042f4daabe1
MatiBiela <mateuszbiela9@gmail.com>
2020-04-24 08:46:57 +0200

    commit

---
+++ josn.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	
+	string imie="Mateusz";
+	string nazwisko="Biela";
+	
+	
+	ofstream wyjscie;
+	wyjscie.open("out.json");
+
+
+	
+	plik<<"[\n{\n\imie":\""<<imie;
+	plik<<"[\n{\n\naziwsko":\""<<nazwisko;
+	plik.close();
+	
+	plik.open("c:\\a.txt")
+		{
+			cout<<imie,nazwisko;
+		}
+	
+	
+	
+	
+	
+	cout<<endl;
+	system ("PAUSE");	
+	
+	
+	
+	
+	
+	return 0;
+}
